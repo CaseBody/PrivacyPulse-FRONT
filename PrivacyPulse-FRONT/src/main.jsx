114fd68cca35a7b9b994b36c73afbde5db419a0b
Rick Rietbergen <rbps3.2016@gmail.com>
2023-05-10 17:53:01 +0200

    routing "/header" load component for header.

--- PrivacyPulse-FRONT/src/main.jsx
+++ PrivacyPulse-FRONT/src/main.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import App from "./App.jsx";
+import Header from "./component/header.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <div>Hello world!</div>,
-	},
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/header",
+    element: <Header />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
