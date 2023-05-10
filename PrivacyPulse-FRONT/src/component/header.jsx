607b896545b41c19fa3291a49c4be73361f1bc8c
Rick Rietbergen <rbps3.2016@gmail.com>
2023-05-10 18:28:54 +0200

    startup header

--- PrivacyPulse-FRONT/src/component/header.jsx
+++ PrivacyPulse-FRONT/src/component/header.jsx
@@ -1,5 +1,35 @@
-const Header = () => {
-    return "header page";
+import React from "react";
+import { AppBar, Toolbar, IconButton, Typography, Button, Avatar, } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+  avatar: {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+function Header() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Privacy Pulse
+          </Typography>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">contact</Button>
+          <Avatar alt="User Avatar" src="/" />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default Header;
